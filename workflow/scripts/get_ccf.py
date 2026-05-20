127ab6373a662f680f7d77e9eff962052fabfaad
Dylan <dylan.house@tuta.com>
2026-05-20 13:26:47 +0100

    Scripts to calculate ccf and plot clonal tree

---
+++ workflow/scripts/get_ccf.py
@@ -0,0 +1,75 @@
+import argparse
+import numpy as np
+import pandas as pd
+
+
+
+def get_ccf(mutation_to_clone, vaf_matrix, filepath):
+    """
+    Calculates the cancer cell fraction (CCF) & the clonal composition in
+    cancer samples from outputs of fastBE cluster. 
+
+        Args:
+            mutation_to_clone: CSV file containing cluster to mutation data
+            vaf_matrix: variant allele frequency matrix in a .txt format according to fastBE's specifications
+            filepath: The path to output.
+        Returns:
+            A CSV file containing the CCF data per clone and sample.
+    """
+    clusters_df = pd.read_csv(mutation_to_clone)
+    clusters_df = clusters_df.sort_values("mutation", ascending = True).reset_index(drop = True)
+    unique_clusters = sorted(clusters_df["clone"].unique())
+    cluster_to_idx = {
+        cluster: idx for idx, cluster in enumerate(unique_clusters)
+    }
+
+    variants_per_cluster = clusters_df["clone"].value_counts()
+    cluster_counts_vector = np.array(
+        [variants_per_cluster[clone] for clone in unique_clusters]
+    )
+
+    vaf_matrix = pd.read_csv(vaf_matrix, sep = " ", header = None).values
+    num_samples, num_variants = vaf_matrix.shape
+
+
+    num_clusters = len(unique_clusters)
+    sum_matrix = np.zeros((num_samples, num_clusters))
+
+    # Iterates over matrix of zeros though variant and sample index and replaces value with that of the VAF matrix value. We then find the clone it belongs to.
+    for sample_idx in range(num_samples):
+        for variant_idx in range(num_variants):
+
+            vaf_val = vaf_matrix[sample_idx, variant_idx]
+
+            # Find which cluster this variant position belongs to
+            cluster_name = clusters_df.loc[variant_idx, "clone"]
+            cluster_idx = cluster_to_idx[cluster_name]
+
+
+            sum_matrix[sample_idx, cluster_idx] += vaf_val
+
+    avg_vaf_matrix = sum_matrix / cluster_counts_vector
+
+    # Multiply by 2 to get CCF
+    ccf_matrix = avg_vaf_matrix * 2
+    ccf_df = pd.DataFrame(ccf_matrix, columns=unique_clusters)
+    ccf_df.index.name = "Sample_Index"
+
+    ccf_df.to_csv(filepath)
+    print("CCF file produced in specified directory")
+
+
+def main():
+    parser = argparse.ArgumentParser("Calculates the cancer cell fraction from fastBE's cluster output and VAF matrix")
+    parser.add_argument('Cluster_Variant', help = "The cluster-variant .csv file")
+    parser.add_argument('VAF_Matrix', help = "The variant allele frequency .txt file")
+    parser.add_argument('output', help = "Filepath to output")
+    args = parser.parse_args()
+
+    if args.Cluster_Variant and args.VAF_Matrix:
+        get_ccf(args.Cluster_Variant, args.VAF_Matrix, args.output)
+    else:
+        raise ValueError("Ensure to input both cluster-variant csv file from fastBE output & the VAF .txt matrix in fastBE format")
+        
+if __name__ == "__main__":
+    main()
---
+++ workflow/scripts/plot_clone.py
@@ -0,0 +1,76 @@
+import argparse
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
+import pandas as pd
+
+
+def get_clone(cluster_file):
+    """
+    Reads input csv file and returns a pandas series 
+    """
+    df = pd.read_csv(cluster_file, header = None)
+    return df.set_index(0)[1]
+
+
+def build_and_draw_clone_tree(adjacency_list_file, mutation_to_clone, ax=None):
+    """
+    Parses mutation relationships, collapses them into clones, and plots the tree.
+
+        Args:
+            adjacency_list_file: File that contains the adjacency list output from fastbe search
+            mutation_to_clone: CSV file containing cluster to mutation data
+        Returns:
+            A clonal tree
+                
+    """
+    clone_tree = nx.DiGraph()
+
+    with open(adjacency_list_file, "r") as f:
+        for line in f:
+            if line.startswith("#") or not line.strip():
+                continue
+
+            mutations = line.split()
+            # The first mutation is the ancestral/parent variant
+            ancestor_clone = mutation_to_clone.get(mutations[0])
+
+            # Subsequent mutations on the line are descendent variants
+            for descendent_mut in mutations[1:]:
+                descendent_clone = mutation_to_clone.get(descendent_mut)
+
+                # Connect only if the mutation meets a distinct sub-clone
+                if ancestor_clone != descendent_clone:
+                    clone_tree.add_edge(ancestor_clone, descendent_clone)
+
+    # Draw the phylogenetic tree
+    pos = graphviz_layout(clone_tree, prog="dot")
+    labels = {node: node for node in clone_tree.nodes()}
+    nx.draw(clone_tree, pos, with_labels=False, arrows=True, ax=ax, node_color="lightblue", node_size=800)
+    nx.draw_networkx_labels(clone_tree, pos, labels, font_size=10, ax=ax)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Reconstruct and plot clonal evolutionary trees from mutation adjacency lists."
+    )
+    parser.add_argument("--cluster",required=True, help="CSV file mapping individual mutations to their designated clone/cluster assignment.")
+    parser.add_argument("adjacency_lists",help="One or more text files containing mutation adjacency hierarchies.",nargs="+")
+    args = parser.parse_args()
+
+    # Map unique clones to mutations
+    mutation_to_clone = get_clone(args.cluster)
+
+    fig, axes = plt.subplots(figsize=(8,6))
+    if len(args.adjacency_lists) == 1:
+        axes = [axes]
+
+    for i, adjacency_list_file in enumerate(args.adjacency_lists):
+        build_and_draw_clone_tree(adjacency_list_file, mutation_to_clone, axes[i])
+
+    output_png = "clonal_lineage_trees.png"
+    plt.savefig(output_png, dpi=300)
+
+
+if __name__ == "__main__":
+    main()
